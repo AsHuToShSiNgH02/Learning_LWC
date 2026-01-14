73c8b4bd6658862a775c778e786d8038ecf39a02
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2026-01-14 18:09:15 +0530

    modified few fields

--- force-app/main/default/lwc/customEventChild/customEventChild.js
+++ force-app/main/default/lwc/customEventChild/customEventChild.js
@@ -4,12 +4,14 @@ export default class CustomEventChild extends LightningElement {
     showVariable;
 
     showVariableMethod(){
+        console.log('this is called in child');
         this.showVariable = 'Opportunit in child';
 
         //This is custom event creation and dispatching
         const event = new CustomEvent('showvariableparent',{
             detail : this.showVariable
-        })
+        });
+        console.log('event created in child' + event.detail);
 
         this.dispatchEvent(event);
     }
